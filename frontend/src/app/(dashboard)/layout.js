ffa5df5a5e0f73a6559f8de22e685ba6e2efc567
Chizycodes <okwudirejoy@gmail.com>
2023-11-23 16:10:09 +0100

    Fix issue

--- frontend/src/app/(dashboard)/layout.js
+++ frontend/src/app/(dashboard)/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className="">
-				<DashboardLayout children={children} />
+				<DashboardLayout>{children}</DashboardLayout>
 			</body>
 		</html>
 	);
