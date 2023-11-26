d6b141d5473eef74a04e69a30cd0a2123093b2d7
Chizycodes <okwudirejoy@gmail.com>
2023-11-26 13:00:57 +0100

    fix

--- frontend/src/utils/withAuth.jsx
+++ frontend/src/utils/withAuth.jsx
@@ -2,13 +2,10 @@
 import { redirect } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { useUser } from '@/context/context-provider';
-import { usePathname } from 'next/navigation';
 
 export default function withAuth(Component) {
 	return function WithAuth(props) {
 		const { state } = useUser();
-		const pathname = usePathname();
-		const [isLoading, setIsLoading] = useState(true);
 
 		useEffect(() => {
 			if (state.isAuth !== null) {
@@ -16,23 +13,20 @@ export default function withAuth(Component) {
 					redirect('/login');
 				}
 			}
-			console.log(state.isAuth);
 		}, [state.isAuth]);
 
-		if (state.isAuth === false) {
-			return null;
+		if (!state.isAuth) {
+			return (
+				<div className="w-screen h-screen flex items-center justify-center gap-3">
+					<h1 className="text-xl">DiagnoSync AI</h1>
+					<span className="loading loading-bars loading-lg"></span>
+				</div>
+			);
 		}
 
 		return (
 			<>
-				{state.isAuth === null ? (
-					<div className="w-screen h-screen flex items-center justify-center gap-3">
-						<h1 className="text-xl">DiagnoSync AI</h1>
-						<span className="loading loading-bars loading-lg"></span>
-					</div>
-				) : (
-					<Component {...props} />
-				)}
+				<Component {...props} />
 			</>
 		);
 	};
